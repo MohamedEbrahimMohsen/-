368d16c41d3a0fa9b40dc63f43c082ec242ae14d
Mohamed Mohsen <Mohamed.Ebrahim.Mohsen@gmail.comgit config --global user.name MohamedEbrahimMohsen>
2018-06-09 16:04:22 +0200

    Initial Commit

---
+++ القرآن الكريم كامل بصوت جميع القراء/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  القرآن الكريم كامل بصوت جميع القراء
+//
+//  Created by Mohamed Mohsen on 6/9/18.
+//  Copyright © 2018 Mohamed Mohsen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
